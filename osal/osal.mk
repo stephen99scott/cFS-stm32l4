5b6246cc29bbbc7d8f7a825b4f2e0b9029094cc5
Stephen <stephen99scott@gmail.com>
2022-03-02 14:01:46 -0500

    Add the osal bsp layer

--- osal/osal.mk
+++ osal/osal.mk
@@ -1,26 +1,36 @@
 OSAL_DIR = osal
 
-OSAL_SHARED_DIR = $(OSAL_DIR)/shared
-OSAL_FREERTOS_DIR = $(OSAL_DIR)/freertos
+OSAL_OS_DIR = $(OSAL_DIR)/os
+OS_SHARED_DIR = $(OSAL_OS_DIR)/shared
+OS_FREERTOS_DIR = $(OSAL_OS_DIR)/freertos
+OS_PORTABLE_DIR = $(OSAL_OS_DIR)/portable
 
-OSAL_INC =  -I$(OSAL_FREERTOS_DIR)/inc \
-			-I$(OSAL_SHARED_DIR)/inc \
-			-I$(OSAL_DIR)/inc \
+OSAL_BSP_DIR = $(OSAL_DIR)/bsp
+BSP_SHARED_DIR = $(OSAL_BSP_DIR)/shared
+BSP_FREERTOS_DIR = $(OSAL_BSP_DIR)/freertos
+
+OSAL_INC =  -I$(OS_FREERTOS_DIR)/inc \
+			-I$(OS_SHARED_DIR)/inc \
+			-I$(BSP_SHARED_DIR)/inc \
+			-I$(OSAL_OS_DIR)/inc \
 			-I$(CONFIG_DIR)
 
-OSAL_FREERTOS_SRC = \
-	$(OSAL_FREERTOS_DIR)/src/os-impl-binsem.c \
-	$(OSAL_FREERTOS_DIR)/src/os-impl-common.c \
-	$(OSAL_FREERTOS_DIR)/src/os-impl-countsem.c
+OSAL_IMPL_SRC = \
+	$(OS_FREERTOS_DIR)/src/os-impl-binsem.c \
+	$(OS_FREERTOS_DIR)/src/os-impl-common.c \
+	$(OS_FREERTOS_DIR)/src/os-impl-countsem.c \
+	$(OS_PORTABLE_DIR)/os-impl-no-loader.c \
+	$(OS_PORTABLE_DIR)/os-impl-no-network.c \
+	$(OS_PORTABLE_DIR)/os-impl-no-shell.c \
+	$(OS_PORTABLE_DIR)/os-impl-no-sockets.c \
+	$(OS_PORTABLE_DIR)/os-impl-no-symtab.c
 
-OSAL_SRC =  $(OSAL_SHARED_SRC) $(OSAL_FREERTOS_SRC)
-
-OSAL_OBJS = $(notdir $(OSAL_SRC:%.c=%.o))
+OSAL_SRC =  $(shell find $(OS_SHARED_DIR)/src -name '*.c')
 
 OSAL_OBJ = build/osal.o
 
 osal: $(OSAL_OBJ)
 
-$(OSAL_OBJ): $(OSAL_SRC)
+$(OSAL_OBJ): $(OSAL_SRC) $(OSAL_IMPL_SRC)
 	@echo "\033[0;32mCompiling OSAL...\033[0m"
 	@$(GCC) $^ -o $@ -r $(CFLAGS) $(OSAL_INC) $(FREERTOS_INC)
